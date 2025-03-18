35c35e37265e25c14b012fbe5ebeb8a4ef1c4137
SWARAJ THAKARE <137782161+Swarajthakare@users.noreply.github.com>
2025-03-18 23:48:14 +0530

    Add files via upload

---
+++ 1 Introduction to seaborn.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### What is seaborn?
+# - Seaborn is a Python library for data visualization build on top of the matplotlib python library.
+# - Use for statistical graphics
+
+# ### Seaborn Graphics
+# - Heatmap
+# - Pair plot
+# - Facet grid
+# - Bar plot
+# - Scatterplot
+# - Line plot
+# - Distplot
+# - Box plot
+# - Violin plot
+
+# ### Seaborn VS Matplotlib
+# - builtin function
+
+# ### Dependency of seaborn
+# - Python
+# - numpy
+# - pandas
+# - matplotlib
+# - scipy
+
+# ### Installation of seaborn
+# - pip install seaborn
+
+# ### Importing Seaborn
+# 
+# #matplotlib
+# - from matplotlib import pyplot as plt
+# - or matplotlib.pyplot as plt
+#  
+# #seaborn
+# - import seaborn as sns
+
+# ### Study materials
+
+# In[ ]:
+
+
+
+
